5fe1e67b03665158ab7c98369d5ec9fba7023461
Pavlo Nazarchuk <pavlo.escola@gmail.com>
2026-01-08 11:13:55 +0000

    [MG-main] fix backend connection

--- src/useAPI.js
+++ src/useAPI.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = process.env.API_BASE_URL || 'http://localhost:3001';
+const API_BASE_URL = process.env.API_BASE_URL || 'http://backend:3001';
 
 export const login = async (username, password) => {
   try {
